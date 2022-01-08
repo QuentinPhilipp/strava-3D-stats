2108d6f569f55ecdbf88a67da56823b0f94a08db
Quentin Philipp <philippquentin6@gmail.com>
2022-01-08 23:20:06 +0100

    build components in batch

--- src/main.js
+++ src/main.js
@@ -4,48 +4,72 @@ import {
     MeshBasicMaterial,
     BoxGeometry,
     WebGLRenderer,
-    Scene
+    Scene,
+    AxesHelper,
+    EdgesGeometry,
+    LineBasicMaterial,
+    LineSegments
   } from "../vendor/three/build/three.module.js";
   
 import { OrbitControls } from "../vendor/three/examples/jsm/controls/OrbitControls.js";
-  
+
+const daysInWeek = 7;
+const weeks = 52;
+const borderWidth = 2;
 const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const scene = new Scene();
-
 const camera = new PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-
 const controls = new OrbitControls( camera, renderer.domElement );
 
 
-const geometry = new BoxGeometry();
-const bottomGeometry = new BoxGeometry(10, 1, 5);
-const materialRed = new MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-const materialWhite = new MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
+const bottomGeometry = new BoxGeometry(daysInWeek + borderWidth, 1, weeks + borderWidth);
+const materialRed = new MeshBasicMaterial( { color: 0xff0000 } );
+const materialWhite = new MeshBasicMaterial( { color: 0xa6a6a6 } );
 const materialGreen = new MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 const materialBlue = new MeshBasicMaterial( { color: 0x0000ff, wireframe: true  } );
 
 
-var cube = new Mesh( geometry, materialRed );
-scene.add( cube );
-cube.position.x = 2;
-cube.position.y = 1;
-var cube2 = new Mesh( geometry, materialGreen );
-scene.add( cube2 );
-cube2.position.y = 3;       
-var cube3 = new Mesh( geometry, materialBlue );
-scene.add( cube3 );
-cube3.position.z = 2;   
-cube3.position.y = 1;
+const numberOfDays = 365;
+const cubeCenterOffset = 0.5;
+const xOffset = daysInWeek/2;
+const yOffset = weeks/2;
+
+for (let week = 0; week < weeks; week++) {
+    for (let dayOfWeek = 0; dayOfWeek < daysInWeek; dayOfWeek++) {
+        var height = Math.floor(Math.random() * 5)
+        const geometry = new BoxGeometry(1, height, 1);
+        var cube = new Mesh( geometry, materialRed );
+        cube.position.set(dayOfWeek + cubeCenterOffset - xOffset, height/2, week + cubeCenterOffset - yOffset);
+        scene.add( cube );
+
+        var geo = new EdgesGeometry( cube.geometry );
+        var mat = new LineBasicMaterial( { color: 0x000000, linewidth: 10 } );
+        var wireframe = new LineSegments( geo, mat );
+        wireframe.renderOrder = 1; // make sure wireframes are rendered 2nd
+        cube.add( wireframe );
+
+    }
+}
+
+var axesHelper = new AxesHelper( 5 );
+scene.add( axesHelper );
 
 
 let center = new Mesh( bottomGeometry, materialWhite);
 scene.add( center );
 
+var geo = new EdgesGeometry( center.geometry );
+var mat = new LineBasicMaterial( { color: 0x000000, linewidth: 10 } );
+var wireframe = new LineSegments( geo, mat );
+wireframe.renderOrder = 1; // make sure wireframes are rendered 2nd
+center.add( wireframe );
+
+
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 20, 50 );
+camera.position.set( daysInWeek/2, 20, 50 );
 controls.update();
 
 function animate() {
