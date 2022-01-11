c2bd3dd71d77d7be4f813461b0d8ad799139fb50
Quentin Philipp <philippquentin6@gmail.com>
2022-01-11 19:46:21 +0100

    Strava authentication to get user access_token

--- backend/main.js
+++ backend/main.js
@@ -1,14 +1,45 @@
-const strava = require('strava-v3');
+var strava = require('strava-v3');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
 require('dotenv').config()
 
-console.log(process.env.STRAVA_ACCESS_TOKEN, process.env.STRAVA_CLIENT_ID, process.env.STRAVA_CLIENT_SECRET);
+strava.config.access_token = process.env.STRAVA_ACCESS_TOKEN;
+strava.config.client_id = process.env.STRAVA_CLIENT_ID;
+strava.config.client_secret = process.env.STRAVA_CLIENT_SECRET;
+strava.config.redirect_uri = process.env.STRAVA_REDIRECT_URI;
 
-async function test() {
-    const payload = await strava.athlete.get({'access_token': process.env.STRAVA_ACCESS_TOKEN}, function(err, payload, limits) {
-        console.log("Error: ", err);
-        console.log("Payload: ", payload);
-        console.log("Limits: ", limits);
-    })
-}
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname+'/static/index.html'));
+});
 
-test();
+app.get("/login", function (req, res) {
+    res.redirect(strava.oauth.getRequestAccessURL({scope: "activity:read_all"}));
+});
+
+
+app.get("/exchange_token", (req, res) => {
+    // Get short-lived access_token from Strava based on received code and scope
+    let authenticationCode = req.query.code;
+    let scope = req.query.scope;
+    console.log(authenticationCode, scope);
+
+    if (scope === "activity:read_all") {
+        // Correct scope, continue login workflow
+        strava.oauth.getToken(code, function (err, payload, connectionIds) {
+            console.log("Ids:", connectionIds);
+            // TODO: Complete error handling
+        });
+    }
+    else {
+        console.log("Scope not validated by user");
+        // TODO: Complete error handling
+    }
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+});
